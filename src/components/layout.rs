62083fb8c747fda6f1e372c099182624cd4a230f
Liam Bury <liamdev631@gmail.com>
2026-02-16 12:12:30 -0900

    feat(layout): implement tabbed navigation with drag-and-drop support
    - Add TabEntry struct and route_to_tab mapping function
    - Replace single active file display with state-managed tabs collection
    - Add drag-and-drop reordering for tabs
    - Implement tab closing with automatic route fallback
    - Use effect to automatically open new tabs when navigating to new routes

--- src/components/layout.rs
+++ src/components/layout.rs
@@ -4,6 +4,55 @@ use crate::router::Route;
 use crate::components::sidebar::Sidebar;
 use crate::components::terminal::Terminal;
 
+#[derive(Clone, PartialEq)]
+struct TabEntry {
+    route: Route,
+    label: String,
+    icon: Html,
+}
+
+fn route_to_tab(route: &Route) -> TabEntry {
+    match route {
+        Route::Home => TabEntry {
+            route: Route::Home,
+            label: "home.html".to_string(),
+            icon: html! { <img src="https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_html.svg" class="file-icon-img" alt="html" /> },
+        },
+        Route::About => TabEntry {
+            route: Route::About,
+            label: "about.html".to_string(),
+            icon: html! { <img src="https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_html.svg" class="file-icon-img" alt="html" /> },
+        },
+        Route::Blog => TabEntry {
+            route: Route::Blog,
+            label: "blog".to_string(),
+            icon: html! { <img src="https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/default_folder.svg" class="file-icon-img" alt="blog" /> },
+        },
+        Route::Portfolio => TabEntry {
+            route: Route::Portfolio,
+            label: "portfolio".to_string(),
+            icon: html! { <img src="https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/default_folder.svg" class="file-icon-img" alt="portfolio" /> },
+        },
+        Route::Project { id } => match id.as_str() {
+            "through-their-eyes" => TabEntry {
+                route: Route::Project { id: id.clone() },
+                label: "through_their_eyes.proj".to_string(),
+                icon: html! { <img src="https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_vcxproj.svg" class="file-icon-img" alt="proj" /> },
+            },
+            _ => TabEntry {
+                route: Route::Project { id: id.clone() },
+                label: "unknown.proj".to_string(),
+                icon: html! { <img src="https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/default_file.svg" class="file-icon-img" alt="file" /> },
+            },
+        },
+        Route::NotFound => TabEntry {
+            route: Route::NotFound,
+            label: "404.rs".to_string(),
+            icon: html! { <img src="https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_rust.svg" class="file-icon-img" alt="rs" /> },
+        },
+    }
+}
+
 #[derive(Properties, PartialEq)]
 pub struct LayoutProps {
     pub children: Html,
@@ -11,19 +60,23 @@ pub struct LayoutProps {
 
 #[function_component(Layout)]
 pub fn layout(props: &LayoutProps) -> Html {
+    let navigator = use_navigator().unwrap();
     let route = use_route::<Route>().unwrap_or(Route::Home);
-    
-    let (active_file, active_icon_html) = match route {
-        Route::Home => ("home.html", html! { <img src="https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_html.svg" class="file-icon-img" alt="html" /> }),
-        Route::About => ("about.html", html! { <img src="https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_html.svg" class="file-icon-img" alt="html" /> }),
-        Route::Blog => ("blog", html! { <img src="https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/default_folder.svg" class="file-icon-img" alt="blog" /> }),
-        Route::Portfolio => ("portfolio", html! { <img src="https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/default_folder.svg" class="file-icon-img" alt="portfolio" /> }),
-        Route::Project { id } => match id.as_str() {
-            "through-their-eyes" => ("through_their_eyes.proj", html! { <img src="https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_vcxproj.svg" class="file-icon-img" alt="proj" /> }),
-            _ => ("unknown.proj", html! { <img src="https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/default_file.svg" class="file-icon-img" alt="file" /> }),
-        },
-        Route::NotFound => ("404.rs", html! { <img src="https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_rust.svg" class="file-icon-img" alt="rs" /> }),
-    };
+    let tabs = use_state(|| vec![route_to_tab(&route)]);
+    let drag_index = use_state(|| None::<usize>);
+
+    {
+        let tabs = tabs.clone();
+        let route = route.clone();
+        use_effect_with(route.clone(), move |_| {
+            let mut next = (*tabs).clone();
+            if !next.iter().any(|tab| tab.route == route) {
+                next.push(route_to_tab(&route));
+                tabs.set(next);
+            }
+            || ()
+        });
+    }
 
     html! {
         <div class="vscode-layout">
@@ -49,11 +102,82 @@ pub fn layout(props: &LayoutProps) -> Html {
             
             <div class="editor-area">
                 <div class="tab-bar">
-                    <div class="tab active">
-                        { active_icon_html }
-                        { active_file }
-                        <span class="tab-close" style="margin-left: 10px; opacity: 0.5;">{ "×" }</span>
-                    </div>
+                    { for (*tabs).iter().cloned().enumerate().map(|(index, tab)| {
+                        let is_active = tab.route == route;
+                        let navigator = navigator.clone();
+                        let tabs = tabs.clone();
+                        let route_current = route.clone();
+                        let drag_index = drag_index.clone();
+                        let tab_route = tab.route.clone();
+                        let tab_route_close = tab.route.clone();
+
+                        let navigator_for_click = navigator.clone();
+                        let on_click = Callback::from(move |_| {
+                            navigator_for_click.push(&tab_route);
+                        });
+
+                        let drag_index_start = drag_index.clone();
+                        let on_drag_start = Callback::from(move |event: DragEvent| {
+                            event.prevent_default();
+                            drag_index_start.set(Some(index));
+                        });
+
+                        let on_drag_over = Callback::from(|event: DragEvent| {
+                            event.prevent_default();
+                        });
+
+                        let tabs_for_drop = tabs.clone();
+                        let drag_index_for_drop = drag_index.clone();
+                        let on_drop = Callback::from(move |event: DragEvent| {
+                            event.prevent_default();
+                            if let Some(from) = *drag_index_for_drop {
+                                let to = index;
+                                if from != to {
+                                    let mut next = (*tabs_for_drop).clone();
+                                    let item = next.remove(from);
+                                    next.insert(to, item);
+                                    tabs_for_drop.set(next);
+                                }
+                            }
+                            drag_index_for_drop.set(None);
+                        });
+
+                        let tabs_for_close = tabs.clone();
+                        let navigator_for_close = navigator.clone();
+                        let on_close = Callback::from(move |event: MouseEvent| {
+                            event.stop_propagation();
+                            let mut next = (*tabs_for_close).clone();
+                            if let Some(pos) = next.iter().position(|t| t.route == tab_route_close) {
+                                next.remove(pos);
+                                tabs_for_close.set(next.clone());
+                                if route_current == tab_route_close {
+                                    let new_route = if pos > 0 {
+                                        next.get(pos - 1)
+                                    } else {
+                                        next.get(0)
+                                    }
+                                    .map(|t| t.route.clone())
+                                    .unwrap_or(Route::Home);
+                                    navigator_for_close.push(&new_route);
+                                }
+                            }
+                        });
+
+                        html! {
+                            <div
+                                class={classes!("tab", if is_active { "active" } else { "" })}
+                                draggable="true"
+                                onclick={on_click}
+                                ondragstart={on_drag_start}
+                                ondragover={on_drag_over}
+                                ondrop={on_drop}
+                            >
+                                { tab.icon.clone() }
+                                { tab.label.clone() }
+                                <span class="tab-close" style="margin-left: 10px; opacity: 0.5;" onclick={on_close}>{ "×" }</span>
+                            </div>
+                        }
+                    }) }
                 </div>
                 <div class="content-container">
                     { props.children.clone() }
